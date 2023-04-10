4ddd0c114bf7ad0ea7d2031ed636f2b2c2d6e3fa
PriyaBalaDharshini <125110897+PriyaBalaDharshini@users.noreply.github.com>
2023-04-10 16:20:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function GoogleForm(){
+    
+        var firstname = document.getElementById("Firstname").value;
+        var middlename = document.getElementById("Middlename").value;
+        var lastname = document.getElementById("Lastname").value;
+        var mail = document.getElementById("Email").value;
+    console.log(firstname);
+    console.log(middlename);
+    console.log(lastname);
+    console.log(mail);
+}
